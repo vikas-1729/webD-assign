b269afe48d4ac9718aa329da63a5fbeb40c36276
vikas-1729 <53541816+vikas-1729@users.noreply.github.com>
2020-06-01 15:30:14 +0530

    correct array function

--- superhero/javascript/main.js
+++ superhero/javascript/main.js
@@ -116,18 +116,20 @@ function start(){
 					$('#'+id1).click(()=>{//clciking on add favourite
 						let favouriteHeroes=JSON.parse(localStorage.getItem('allFavouriteHeroes'));
 						let add=true;//this add will be false if item is already added
-						console.log("ok i am in");
+						
 						{
 						let array=JSON.parse(localStorage.getItem('allFavouriteHeroes'));
-						for(let j of array){
+						if(array.length>0){
+							for(let j of array){
 							if(j.id===i.id){
 								add=false;
 								break;
 							}
+						      }
 						}
 
 					}
-						console.log("add",add);
+						
 							if(add==true){
 							let obj=new Object();
 							obj['id']=i.id;
